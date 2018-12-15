d1184c7f9f7fe99c6afb7ebf48507d3d543a4460
gzeinnumer <gzeinnumer@github.com>
2018-12-15 22:53:41 +0700

    master

---
+++ app/src/main/java/com/zein/firebasedemo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.zein.firebasedemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btCreateDB;
+    private Button btViewDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btCreateDB = (Button) findViewById(R.id.bt_createdata);
+        btViewDB = (Button) findViewById(R.id.bt_viewdata);
+
+        btCreateDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // kelas yang akan dijalankan ketika tombol Create/Insert Data diklik
+                startActivity(FirebaseDBCreateActivity.getActIntent(MainActivity.this));
+            }
+        });
+        btViewDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // kode untuk tutorial selanjutnya
+                Intent intent = new Intent(getApplicationContext(), FirebaseDBReadActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
